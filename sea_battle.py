f7de67c6380de186e96f48add1a98615a8f85890
Konstantin <123>
2025-03-30 23:30:35 +0900

    next

--- sea_battle.py
+++ sea_battle.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import random
+import time
 
 
 class Windows(tk.Tk):
@@ -27,40 +28,79 @@ class OnePlayer(tk.Toplevel):
         tk.Toplevel.__init__(self, parent)
         self.wm_attributes("-topmost", 1)
 
-        canvas_x = 400
-        canvas_y = 400
-        n_x = n_y = 10 #количество ячеек
-        step_x = canvas_x // n_x
-        step_y = canvas_y // n_y
-        print(step_x, step_y)
+        self.canvas_x = 400
+        self.canvas_y = 400
+        self.n_x = self.n_y = 10 #количество ячеек
+        self.step_x = self.canvas_x // self.n_x
+        self.step_y = self.canvas_y // self.n_y
+
 
         label = tk.Label(self, text="Добро пожаловать в игру морской бой")
-        label.grid(row=1, column=0, columnspan=2, sticky="n")
+        label.grid(row=1, column=0, columnspan=3, sticky="n")
 
-        field_one = tk.Canvas(self, bg = "white",
-                              width=canvas_x,
-                              height=canvas_y,
+        self.field_one = self.field_creation(row=2, col=0)
+        self.field_one_coord = self.coord_fild()
+        print(self.field_one_coord)
+        field_two = self.field_creation(row=2, col=2)
+
+        self.field_one.bind("<Button-1>", self.field_coordinates)
+
+        tk.Button(self, text="Очистить поле", command=self.clear_fild).grid(row=4, column=0, sticky="n")
+
+    def clear_fild(self):
+        self.field_one.delete("X")
+        self.field_one_coord = self.coord_fild()
+
+
+    def field_coordinates(self, event):
+        n_x = event.x // self.step_x
+        n_y = event.y // self.step_y
+        self.field_one_coord[n_x, n_y] = "X"
+        self.update_fild()
+
+
+    def update_fild(self):
+        for coord in self.field_one_coord:
+            if self.field_one_coord[coord] == "X":
+                n_x = coord[0]
+                n_y = coord[1]
+                self.field_one.create_line(n_x * self.step_x,
+                                           n_y * self.step_y,
+                                           n_x * self.step_x + self.step_x,
+                                           n_y * self.step_y + self.step_y,
+                                           fill="red", tags=["X"])
+                self.field_one.create_line(n_x * self.step_x + self.step_x,
+                                           n_y * self.step_y,
+                                           n_x * self.step_x,
+                                           n_y * self.step_y + self.step_y,
+                                           fill="red", tags=["X"])
+                self.update_idletasks()
+                self.update()
+
+
+
+    def field_creation(self, row, col):
+        field = tk.Canvas(self, bg="white",
+                              width=self.canvas_x,
+                              height=self.canvas_y,
                               highlightthickness=2,
                               highlightbackground='black'
                               )
-        field_one.grid(row=2, column=0, sticky="n")
+        field.grid(row=row, column=col, sticky="n")
 
-        for i in range(n_x):
-            field_one.create_line(0, step_y * i, canvas_x+5, step_y * i)
+        for i in range(self.n_x):
+            field.create_line(0, self.step_y * i, self.canvas_x + 5, self.step_y * i)
 
-        for i in range(n_y):
-            field_one.create_line(step_x * i, 0, step_x * i, canvas_y+5)
+        for i in range(self.n_y):
+            field.create_line(self.step_x * i, 0, self.step_x * i, self.canvas_y + 5)
+        return field
 
-
-        field_two = tk.Canvas(self, bg = "white", width=canvas_x, height=canvas_y)
-        field_two.grid(row=2, column=1, sticky="n")
-
-        for i in range(n_x):
-            field_two.create_line(0, step_y * i, canvas_x, step_y * i)
-
-        for i in range(n_y):
-            field_two.create_line(step_x * i, 0, step_x * i, canvas_y)
-
+    def coord_fild(self):
+        fild = {}
+        for x in range(10):
+            for y in range(10):
+                fild[x, y] = '-'
+        return fild
 
 
 
